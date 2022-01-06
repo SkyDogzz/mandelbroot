d170a7c95978a8005a32ffa21d1a30c03d60394c
SkyDogzz <thomas.stephan@live.fr>
2022-01-06 22:49:37 +0100

    add a test in c to compare the iteration number

---
+++ testC/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+    float x1 = -2.1;
+    float x2 = 0.6;
+    float y1 = -1.2;
+    float y2 = 1.2;
+    int cpt = 0;
+
+    float c_r, c_i, z_r, z_i, tmp;
+    int i;
+
+    int zoom = 100;
+    int iteration_max = 50;
+
+    float image_x, image_y;
+
+    image_x = (x2 - x1) * zoom;
+    image_y = (y2 - y1) * zoom;
+
+    printf("%f -- %f\n", image_x, image_y);
+
+    for (int x = 0; x < image_x; x++)
+    {
+        for (int y = 0; y < image_y; y++)
+        {
+            cpt++;
+
+            c_r = x / zoom + x1;
+            c_i = y / zoom + y1;
+            z_r = 0;
+            z_i = 0;
+            i = 0;
+
+            do
+            {
+                tmp = z_r;
+                z_r = z_r * z_r - z_i * z_i + c_r;
+                z_i = 2 * z_i * tmp + c_i;
+                i = i + 1;
+            } while (z_r * z_r + z_i * z_i < 4 && i < iteration_max);
+        }
+    }
+
+    printf("%d\n", cpt);
+
+    return EXIT_SUCCESS;
+}
